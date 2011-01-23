198b3522535d1d2f4888ea035781cc98a78bc3ed
Adam Hughes <adam@hughes.cc>
2011-01-23 12:18:52 -0500

    Show simple page preview and remove unused artifact.

--- PdfSplitter/SplitterWindow.Designer.cs
+++ PdfSplitter/SplitterWindow.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.open_button = new System.Windows.Forms.Button();
+            this.preview_panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // open_button
+            // 
+            this.open_button.Location = new System.Drawing.Point(12, 12);
+            this.open_button.Name = "open_button";
+            this.open_button.Size = new System.Drawing.Size(75, 23);
+            this.open_button.TabIndex = 0;
+            this.open_button.Text = "Open";
+            this.open_button.UseVisualStyleBackColor = true;
+            this.open_button.Click += new System.EventHandler(this.open_button_Click);
+            // 
+            // preview_panel
+            // 
+            this.preview_panel.AutoScroll = true;
+            this.preview_panel.Location = new System.Drawing.Point(12, 41);
+            this.preview_panel.Name = "preview_panel";
+            this.preview_panel.Size = new System.Drawing.Size(660, 178);
+            this.preview_panel.TabIndex = 1;
+            // 
+            // SplitterWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(684, 482);
+            this.Controls.Add(this.preview_panel);
+            this.Controls.Add(this.open_button);
+            this.Name = "SplitterWindow";
+            this.Text = "PDF Splitter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button open_button;
+        private System.Windows.Forms.Panel preview_panel;
     }
 }
 
--- PdfSplitter/SplitterWindow.cs
+++ PdfSplitter/SplitterWindow.cs
@@ -11,9 +11,86 @@ namespace PdfSplitter
 {
     public partial class SplitterWindow : Form
     {
+        string m_path;
+        PDFLibNet.PDFWrapper m_doc;
+
         public SplitterWindow()
         {
             InitializeComponent();
+        }
+
+        private void open_button_Click(object sender, EventArgs e)
+        {
+            using (var o = new OpenFileDialog())
+            {
+                o.Filter = "PDF Files (*.pdf)|*.pdf";
+
+                if (o.ShowDialog() == DialogResult.OK)
+                {
+                    OpenFile(o.FileName);
+                }
+            }
+        }
+
+        void OpenFile(string path)
+        {
+            var doc = new PDFLibNet.PDFWrapper();
+            doc.UseMuPDF = true;
+            doc.LoadPDF(path);
+
+            m_path = path;
+            m_doc = doc;
+
+            LoadImages();
+        }
+
+        void LoadImages()
+        {
+            int pages = m_doc.PageCount;
+
+            for (int i = 0; i < pages; i++)
+            {
+                Image image = RenderPage(m_doc, i);
+                PictureBox box = new PictureBox();
+                box.Top = 0;
+                box.Left = i * 105;
+                box.Height = 200;
+                box.Width = 100;
+                box.SizeMode = PictureBoxSizeMode.Zoom;
+                box.Image = image;
+                preview_panel.Controls.Add(box);
+            }
+        }
+
+        Image RenderPage(PDFLibNet.PDFWrapper doc, int page)
+        {
+            doc.CurrentPage = page + 1;
+            doc.CurrentX = 0;
+            doc.CurrentY = 0;
+
+            using (var box = new PictureBox())
+            {
+                // have to give the document a handle to render into
+                doc.RenderPage(box.Handle);
+
+                // create an image to draw the page into
+                var buffer = new Bitmap(doc.PageWidth, doc.PageHeight);
+                doc.ClientBounds = new Rectangle(0, 0, doc.PageWidth, doc.PageHeight);
+                using (var g = Graphics.FromImage(buffer))
+                {
+                    var hdc = g.GetHdc();
+                    try
+                    {
+                        doc.DrawPageHDC(hdc);
+                    }
+                    finally
+                    {
+                        g.ReleaseHdc();
+                    }
+                }
+
+                return buffer;
+            }
         }
     }
 }
