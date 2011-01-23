7d5fc361da340e61f89ceb06c2921e7789f9eeae
Adam Hughes <adam@hughes.cc>
2011-01-23 12:31:52 -0500

    Add different preview sizes.

--- PdfSplitter/SplitterWindow.Designer.cs
+++ PdfSplitter/SplitterWindow.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.open_button = new System.Windows.Forms.Button();
             this.preview_panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.preview_size_combo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // open_button
@@ -44,17 +45,34 @@
             // 
             // preview_panel
             // 
+            this.preview_panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.preview_panel.AutoScroll = true;
             this.preview_panel.Location = new System.Drawing.Point(12, 41);
             this.preview_panel.Name = "preview_panel";
             this.preview_panel.Size = new System.Drawing.Size(660, 245);
             this.preview_panel.TabIndex = 2;
             // 
+            // preview_size_combo
+            // 
+            this.preview_size_combo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.preview_size_combo.FormattingEnabled = true;
+            this.preview_size_combo.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.preview_size_combo.Location = new System.Drawing.Point(551, 14);
+            this.preview_size_combo.Name = "preview_size_combo";
+            this.preview_size_combo.Size = new System.Drawing.Size(121, 21);
+            this.preview_size_combo.TabIndex = 3;
+            // 
             // SplitterWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 482);
+            this.Controls.Add(this.preview_size_combo);
             this.Controls.Add(this.preview_panel);
             this.Controls.Add(this.open_button);
             this.Name = "SplitterWindow";
@@ -67,6 +85,7 @@
 
         private System.Windows.Forms.Button open_button;
         private System.Windows.Forms.FlowLayoutPanel preview_panel;
+        private System.Windows.Forms.ComboBox preview_size_combo;
     }
 }
 
--- PdfSplitter/SplitterWindow.cs
+++ PdfSplitter/SplitterWindow.cs
@@ -17,6 +17,24 @@ namespace PdfSplitter
         public SplitterWindow()
         {
             InitializeComponent();
+
+            preview_size_combo.SelectedIndex = 1;
+            preview_size_combo.SelectedIndexChanged += new EventHandler(preview_size_combo_SelectedIndexChanged);
+        }
+
+        void preview_size_combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // preview size changed, reload the images
+            string size = preview_size_combo.Text;
+
+            foreach (Control control in preview_panel.Controls)
+            {
+                PictureBox box = control as PictureBox;
+                if (box != null)
+                {
+                    box.Size = GetPreviewSize(box.Image, size);
+                }
+            }
         }
 
         private void open_button_Click(object sender, EventArgs e)
@@ -48,19 +66,45 @@ namespace PdfSplitter
         {
             int pages = m_doc.PageCount;
 
+            preview_panel.Controls.Clear();
+
             // render each page and stuff them into the preview panel
             for (int i = 0; i < pages; i++)
             {
                 Image image = RenderPage(m_doc, i);
                 PictureBox box = new PictureBox();
-                box.Top = 0;
-                box.Left = i * 105;
-                box.Height = 200;
-                box.Width = 100;
+                box.Size = GetPreviewSize(image, preview_size_combo.Text);
                 box.SizeMode = PictureBoxSizeMode.Zoom;
                 box.Image = image;
                 preview_panel.Controls.Add(box);
             }
+        }
+
+        Size GetPreviewSize(Image image, string preview_size)
+        {
+            int max_width = 150, max_height = 350;
+            switch (preview_size)
+            {
+                case "Small":
+                    max_width = 100;
+                    max_height = 200;
+                    break;
+                case "Medium":
+                    max_width = 150;
+                    max_height = 300;
+                    break;
+                case "Large":
+                    max_width = 250;
+                    max_height = 500;
+                    break;
+            }
+
+            decimal width_ratio = max_width / ((decimal)image.Width);
+            decimal height_ratio = max_height / ((decimal)image.Height);
+
+            if (width_ratio < height_ratio)
+                return new Size((int)(image.Width * width_ratio), (int)(image.Height * width_ratio));
+            return new Size((int)(image.Width * height_ratio), (int)(image.Height * height_ratio));
         }
 
         Image RenderPage(PDFLibNet.PDFWrapper doc, int page)
